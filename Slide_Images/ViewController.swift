45e47f5c91dc7a6dd546b1b39455fe6e8e597151
azdigital <andrew@azdigital.ca>
2016-10-29 18:00:05 -0400

    Initial Commit

---
+++ Slide_Images/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slide_Images
+//
+//  Created by Andrew on 2016-10-29.
+//  Copyright © 2016 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
